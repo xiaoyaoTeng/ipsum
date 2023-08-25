62f2f737986fef13cae6f8bca8e90083efc3e3ab
xiaoyaoTeng <91022948+xiaoyaoTeng@users.noreply.github.com>
2023-08-25 12:15:36 +0800

    Update sshbl-deny.sh

--- sshbl-deny.sh
+++ sshbl-deny.sh
@@ -6,12 +6,12 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
  
 URL=https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt
 HOSTSDENY=/etc/hosts.deny
-TMP_DIR=/dev/shm
+TMP_DIR=/tmp/
 FILE=hosts.deny
  
 cd $TMP_DIR
 
-curl --compressed $URL 2>/dev/null | grep -v "#" | grep -v -E "\s[1-2]$" | cut -f 1  > $FILE
+curl --compressed $URL 2>/dev/null | grep -v -E "\s[1-2]$" | cut -f 1  > $FILE
 sed -i 's/^/ALL:&/g' $FILE
 LINES=`grep "^ALL:" $FILE | wc -l`
  
